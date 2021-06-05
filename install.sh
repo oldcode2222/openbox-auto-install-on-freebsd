1b6e1c493c2bd7c4481a1d5cc340d08f4bf41eb8
oldcode2222 <31588285+oldcode2222@users.noreply.github.com>
2021-06-06 00:47:30 +0900

    update install.sh
    freebsd settings file customize.

--- install.sh
+++ install.sh
@@ -11,14 +11,12 @@
 
 sudo pkg install xorg openbox openbox-arc-theme slim slim-freebsd-black-theme \
      bash fish plank xfce4-desktop xfce4-power-manager tint2 git \
-     ja-fcitx-mozc compton virtualbox-ose-additions firefox thunderbird \
+     ja-fcitx-mozc compton firefox thunderbird \
      sakura emacs vim ja-mozc-el-emacs26 thunar noto noto-basic \
      noto-extra noto-jp ja-font-vlgothic scrot rofi i3lock xautolock nano
 
 sudo Xorg -configure
 sudo mv /root/xorg.conf.new /etc/X11/xorg.conf
-sudo sed -i -e "/^.*Driver/s/mouse/vboxmouse/" /etc/X11/xorg.conf
-sudo sed -i -e "/^.*Driver/s/vesa/vboxvideo/" /etc/X11/xorg.conf
 
 git clone https://github.com/powerline/fonts.git ~/powerline-fonts
 . ~/powerline-fonts/install.sh
@@ -31,12 +29,12 @@ export GTK_IM_MODULE=fcitx\nexport QT_IM_MODULE=fcitx\n\
 export XMODIFIERS=@im=fcitx\n\n/usr/local/bin/mozc start\n\
 fcitx -r -d\ncompton -b\nplank &\nxfdesktop &\n\n\
 setxkbmap -layout jp\nxrandr -s 1024x768\n\
-/usr/local/bin/VBoxClient-all &\n\n\
 tint2 -c ~/.config/tint2/minima/minima.tint2rc &\n\n\
 exec openbox-session" >> ~/.xinitrc
 
 sudo sh -c 'echo -e "dbus_enable=\"YES\"\nhald_enable=\"YES\"\n\
-vboxguest_enable=\"YES\"\nvboxservice_enable=\"YES\"\nntpdate_enable=\"YES\"\n\
+polkitd_enable=\"YES\"\n\
+ntpdate_enable=\"YES\"\n\
 ntpdate_flags=\"-b ntp.nict.jp\"\n\
 slim_enable=\"YES\"" >> /etc/rc.conf'
 
